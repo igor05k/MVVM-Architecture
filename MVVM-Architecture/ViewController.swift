d4c6ade71dcf56406249fe370dcf83ac09db3333
Igor Fernandes <igormedfer@gmail.com>
2022-11-01 17:10:58 -0300

    first commit

---
+++ MVVM-Architecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-Architecture
+//
+//  Created by Igor Fernandes on 01/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
